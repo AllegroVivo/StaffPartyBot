5183c74bfd206dd543e23a230f00ac0ae048f189
Stephanie Phillips <StephBotCoder@gmail.com>
2024-05-05 23:56:21 -0700

    Reverted image link shortening process (for now)

--- Classes/Profiles/Profile.py
+++ Classes/Profiles/Profile.py
@@ -262,8 +262,8 @@ class Profile:
                 return
 
             image_url = await self.bot.dump_image(file)
-            if shortened_url := U.shorten_url(image_url):
-                image_url = shortened_url
+            # if shortened_url := U.shorten_url(image_url):
+            #     image_url = shortened_url
             
             await self._images.add_additional(interaction, image_url, modal.value)
             
